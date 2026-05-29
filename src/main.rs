59d8d2b8c047db91345dc7a35333b734193847a6
God_solder <rezistor@disroot.org>
2026-05-29 12:46:35 +0500

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ use tokio::sync::mpsc;
 use tracing_subscriber;
 use crate::state::AppState;
 use crate::network::{ClientCommand, ServerEvent};
+use iced::{Font, Settings};
 
 mod state;
 mod network;
 mod app;
 
+const APP_FONT_BYTES: &[u8] = include_bytes!("../assets/fonts/NotoSansSymbols-VariableFont_wght.ttf");
+pub const APP_FONT: Font = Font::with_name("NotoSansSymbols-Regular");
+
+const EMOJI_FONT_BYTES: &[u8] = include_bytes!("../assets/fonts/NotoColorEmoji.ttf");
+pub const EMOJI_FONT: Font = Font::with_name("Segoe UI Emoji");
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().init();
     let db_path = env::var("ANON_DB").unwrap_or_else(|_| "./anon.db".into());
-
     let app = AppState::new(&db_path).await?;
-    
-    // Получаем адрес: приоритет у переменной окружения, если её нет — берём из базы данных
+
     let ws_url = env::var("ANON_SERVER").unwrap_or_else(|_| app.server_address.clone());
 
     let (tx, rx) = mpsc::unbounded_channel::<ServerEvent>();
@@ -29,7 +34,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     });
 
-    app::run(app, rx, cmd_tx)?; 
+    let mut settings = Settings::default();
+    settings.fonts.push(std::borrow::Cow::Borrowed(APP_FONT_BYTES));
+    settings.fonts.push(std::borrow::Cow::Borrowed(EMOJI_FONT_BYTES));
+    settings.default_font = APP_FONT;
+
+    app::run(app, rx, cmd_tx)?;
 
     Ok(())
 }
